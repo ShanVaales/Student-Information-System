87f2b15df97fc4dff4c29a67dab8d43ef45d3fea
ShanVaales <shanlal.k@vaales.com>
2024-12-02 18:00:17 +0530

    Student,teacher

---
+++ Model/StudentMedicalDetails.cs
@@ -0,0 +1,13 @@
+﻿using SIS.Model.BaseModel;
+
+namespace SIS.Model
+{
+    public class StudentMedicalDetails : TimeEntity
+    {
+        public int Id { get; set; }
+        public int StudentID { get; set; }
+        public int BloodGroup { get; set; }
+        public double height { get; set; }
+        public double weight { get; set; }
+    }
+}
---
+++ Model/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using SIS.Model.BaseModel;
+
+namespace SIS.Model
+{
+    public class Teacher : TimeEntity
+    {
+        public int Id { get; set; }
+        public string FistName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+        public int Gender { get; set; }
+        public string EmergencyContactNo { get; set; }
+
+        //Professional Details 
+        public string EmployeeID { get; set; }
+        public int Designation { get; set; }
+        public int Department { get; set; }
+        public DateTime DateOfJoin { get; set; }
+        public int Qualification { get; set; }
+        public int Experience { get; set; }
+    }
+}
