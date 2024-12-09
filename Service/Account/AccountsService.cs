e82b5abb8a49b1e43e7e81a71412d59669bfef92
Shanlal K <shanlal.k@vaales.com>
2024-12-09 11:15:08 +0530

    #PasswordHashing,Salt

--- Service/Account/AccountsService.cs
+++ Service/Account/AccountsService.cs
@@ -1,7 +1,28 @@
-﻿namespace SIS.Service.Account
+﻿using SIS.Model;
+using SIS.SISContext;
+
+namespace SIS.Service.Account
 {
-    public class AccountsService
+    public class AccountsService : IAccountsService
     {
+        private readonly SISDbContext _context;
+        public AccountsService( SISDbContext context)
+        {
+            _context = context;
+        }
+        public Task<int> SaveUser(AspUser user)
+        {
+            _context.Add(user);
+            _context.SaveChanges();
+            return Task.FromResult(0);
+        }
+
+        public async Task<AspUser> GetByIdUserName (string userName)
+        {
+            var result =  _context.AspUsers.Where(x => x.Username == userName).FirstOrDefault();
+            return result;
+        }
+
 
     }
 }
--- Service/Account/IAccountsService.cs
+++ Service/Account/IAccountsService.cs
@@ -1,7 +1,10 @@
-﻿namespace SIS.Service.Account
+﻿using SIS.Model;
+
+namespace SIS.Service.Account
 {
     public interface IAccountsService
     {
-
+        Task<int> SaveUser(AspUser user);
+        Task<AspUser> GetByIdUserName(string userName);
     }
 }
